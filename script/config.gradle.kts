c10d78c69bef819b99e618c8c0c4f3e185c6e0d5
Michael Nien <mobile.michael.nien@gmail.com>
2022-01-18 21:57:57 +0800

    新增 ktlint 套件引用

---
+++ script/config.gradle.kts
@@ -0,0 +1,18 @@
+val ktlint by configurations.creating
+
+dependencies {
+    ktlint("com.pinterest:ktlint:0.36.0")
+}
+
+val outputDir = "${project.buildDir}/reports/ktlint/"
+val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+val ktlintCheck by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("src/**/*.kt")
+}
